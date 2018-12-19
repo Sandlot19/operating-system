1ee68b66171a0829da0f1d67ab7ddf22273c712e
John Shamoon <johnshamoon96@gmail.com>
2018-12-18 19:35:23 -0500

    Kernel: Fix Date
    Update the date from December 17, 2018 to Decemmber 18, 2018 because it
    is not December 17, 2018 and it is December 18, 2018.

--- kernel/kernel/main.cc
+++ kernel/kernel/main.cc
@@ -3,7 +3,7 @@
 int main(void) {
   Terminal& term = Terminal::GetTerm();
   term.ClearScreen();
-  term.print("Hello world! This is the %d day of %s!", 17, "December");
+  term.print("Hello world! This is the %d day of %s!", 18, "December");
   while(1);
   return 0;
 }
